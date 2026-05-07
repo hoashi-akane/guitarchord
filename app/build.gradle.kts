9b9a8ab8bcfea5b56a16ab00ae739ea53e76af79
hoashi <akane.hoashi@outlook.jp>
2026-05-08 00:15:56 +0900

    バージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "jp.ahoashi.guitarchord"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10100
-        versionName = "1.1.0"
+        versionCode = 10101
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
